b02cdaf7146572b602f2a8c3621ac5451c92625f
markocurlin <marko.curlin22@gmail.com>
2022-12-20 12:15:45 +0100

    feat: added contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/layouts/Layout';
+
+const Contact = () => {
+  return (
+    <>
+      <Layout>
+        <p>Contact us</p>
+      </Layout>
+    </>
+  );
+};
+
+export default Contact;
