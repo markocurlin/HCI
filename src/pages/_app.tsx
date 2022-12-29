5d428f80d3c3ff2ae5e3be46d1af575c7e45b8c2
markocurlin <marko.curlin22@gmail.com>
2022-12-29 01:23:19 +0100

    feat: favicon againa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,40 +2,29 @@ import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
 import React from 'react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
   return (
     <React.Fragment>
       <Head>
         <meta name="hotelbb" />
-        <link
-          rel="apple-touch-icon"
-          href={`${router.basePath}/apple-touch-icon.png`}
-          key="apple"
-        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" key="apple" />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href={`${router.basePath}/favicon-32x32.png`}
+          href="/favicon-32x32.png"
           key="icon32"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href={`${router.basePath}/favicon-16x16.png`}
+          href="/favicon-16x16.png"
           key="icon16"
         />
-        <link
-          rel="icon"
-          href={`${router.basePath}/favicon.ico`}
-          key="favicon"
-        />
+        <link rel="icon" href="/favicon.ico" key="favicon" />
       </Head>
       <Component {...pageProps} />
     </React.Fragment>
