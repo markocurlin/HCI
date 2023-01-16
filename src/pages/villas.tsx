81d3f4d4eed6f3bb92a5ba4b921ddd9e79c55c46
markocurlin <marko.curlin22@gmail.com>
2023-01-16 11:14:11 +0100

    feat: commit

--- src/pages/villas.tsx
+++ src/pages/villas.tsx
@@ -79,6 +79,157 @@ const Villas = () => {
           </div>
         </div>
 
+        <div className="bg-zinc-100 pb-8 sm:py-12 md:pt-0 lg:px-8">
+          {villas.map((villa) => (
+            <div
+              key={villa.id}
+              ref={villa.id === id ? ref : null}
+              className="bg-zinc-100 pb-8 lg:p-8"
+            >
+              <div className="mx-auto mt-8 grid w-full max-w-screen-md grid-cols-12 md:mt-0 md:max-w-screen-2xl md:items-center lg:mt-16">
+                {villa.id % 2 === 0 && (
+                  <>
+                    <div className="md:col-span-auto z-0 col-span-12 lg:z-20 lg:col-start-6 lg:col-end-13 lg:row-start-1 lg:row-end-1">
+                      <div className="image-padding relative block h-0 w-full overflow-hidden bg-gray-300">
+                        <div
+                          key={`${villa.id}-carousel`}
+                          className="absolute inset-0 h-full w-full object-cover"
+                        >
+                          <Carousel imageList={villa.images} />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="md:col-span-auto relative z-10 col-span-12 -mt-8 px-10 md:px-24 lg:col-start-1 lg:col-end-7 lg:row-start-1 lg:row-end-1 lg:mt-0 lg:px-0">
+                      <div className="bg-white p-4 sm:p-12 md:px-8">
+                        <div className="mx-auto max-w-xl py-8 px-4 sm:px-6 lg:max-w-2xl lg:px-2 xl:py-24">
+                          <div className="container flex flex-col">
+                            <div className="container">
+                              <p className="text-xl font-semibold text-black lg:text-3xl">
+                                {t(`${villa.name}`)}
+                              </p>
+                              <p className="mt-2 text-justify text-sm leading-snug text-black/70 sm:text-base lg:pr-24 xl:pr-32">
+                                {t(`${villa.description1}`)}
+                              </p>
+                            </div>
+                            <div>
+                              <Link
+                                href="/contact/#book"
+                                className="bg-secondary bg-hover-secondary mt-6 inline-flex items-center justify-center whitespace-nowrap border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm"
+                              >
+                                {t('BOOK NOW')}
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+
+                {villa.id % 2 !== 0 && (
+                  <>
+                    <div className="md:col-span-auto z-0 col-span-12 lg:z-20 lg:col-start-1 lg:col-end-8 lg:row-start-1 lg:row-end-1">
+                      <div className="image-padding relative block h-0 w-full overflow-hidden bg-gray-300">
+                        <div
+                          key={`${villa.id}-carousel`}
+                          className="absolute inset-0 h-full w-full object-cover"
+                        >
+                          <Carousel imageList={villa.images} />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="md:col-span-auto relative z-10 col-span-12 -mt-8 px-10 md:px-24 lg:col-start-7 lg:col-end-13 lg:row-start-1 lg:row-end-1 lg:mt-0 lg:px-0">
+                      <div className="bg-white p-4 sm:p-12 md:px-8">
+                        <div className="mx-auto max-w-xl py-8 px-4 sm:px-6 lg:max-w-2xl lg:px-2 lg:pl-24 xl:py-24 xl:pl-32">
+                          <div className="container flex flex-col">
+                            <div className="container">
+                              <p className="text-xl font-semibold text-black lg:text-3xl">
+                                {t(`${villa.name}`)}
+                              </p>
+                              <p className="mt-2 text-justify text-sm leading-snug text-black/70 sm:text-base">
+                                {t(`${villa.description1}`)}
+                              </p>
+                            </div>
+                            <div>
+                              <Link
+                                href="/contact/#book"
+                                className="bg-secondary bg-hover-secondary mt-6 inline-flex items-center justify-center whitespace-nowrap border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm"
+                              >
+                                {t('BOOK NOW')}
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )}
+              </div>
+
+              <div className="mx-auto mt-16 mb-4 grid w-full max-w-screen-md grid-cols-11 justify-items-center px-10 sm:mt-24 md:mb-8 md:mt-16 md:max-w-screen-2xl md:px-24 lg:mt-24">
+                <div className="container col-span-11 mb-16 bg-white p-12 md:mb-16 lg:col-start-1 lg:col-end-6 lg:row-start-1 lg:row-end-1 lg:mb-0">
+                  {villas[0]!.facilities.map((facilitie, index) => (
+                    <>
+                      {index < 5 && (
+                        <div key={`${index}-${facilitie.title}`}>
+                          <div className="flex p-3">
+                            <p className="w-40 p-1 text-sm font-semibold leading-snug text-black/70 sm:w-60 sm:text-lg">
+                              {facilitie.title}
+                            </p>
+                            <ul
+                              key={`${index}-${facilitie}`}
+                              className="ml-0 list-inside list-disc"
+                            >
+                              {facilitie.details.map((details) => (
+                                <li
+                                  key={`${index}-${details}`}
+                                  className="mx-4 w-40 p-1 text-justify text-sm font-normal leading-snug text-black/70 sm:w-60 sm:text-lg"
+                                >
+                                  {details}
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  ))}
+                </div>
+
+                <div className="container col-span-11 bg-white p-12 lg:col-start-7 lg:col-end-12 lg:row-start-1 lg:row-end-1">
+                  {villas[0]!.facilities.map((facilitie, index) => (
+                    <>
+                      {index > 5 && (
+                        <div key={`${index}-${facilitie.title}`}>
+                          <div className="flex p-3">
+                            <p className="w-40 p-1 text-sm font-semibold leading-snug text-black/70 sm:w-60 sm:text-lg">
+                              {facilitie.title}
+                            </p>
+                            <ul
+                              key={`${index}-${facilitie}`}
+                              className="ml-0 list-inside list-disc"
+                            >
+                              {facilitie.details.map((details) => (
+                                <li
+                                  key={`${index}-${details}`}
+                                  className="mx-4 w-40 p-1 text-justify text-sm font-normal leading-snug text-black/70 sm:w-60 sm:text-lg"
+                                >
+                                  {details}
+                                </li>
+                              ))}
+                            </ul>
+                          </div>
+                        </div>
+                      )}
+                    </>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        {/* }
         <div className="bg-zinc-100 pb-8 sm:py-12 lg:px-8">
           <div
             key={villas[0]!.id}
