b055f6a6b5d5f80dc9da42b12e838691508c6b81
Marko <marko.curlin22@gmail.com>
2023-06-17 17:48:57 +0200

    feat: commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* const withBundleAnalyzer = require('@next/bundle-analyzer')({
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
@@ -7,15 +6,7 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
   },
-  images: {
-    domains: ['drive.google.com'],
-  },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
 });
-*/
